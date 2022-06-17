cef8ee0e7ce58b33b83390759f59b165636d6b6d
Madusanka <42802790+sankalk@users.noreply.github.com>
2022-06-17 15:46:45 +0530

    Update dark.js

--- dark.js
+++ dark.js
@@ -21,3 +21,30 @@ function manually_darkmode_toggle() {
   let is_darkmode_enable = element.classList.toggle("dark");
   localStorage.setItem("darkmode", is_darkmode_enable); // Save to localStorage
 }
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark");
+ }
+// On page load set the theme.
+(function() {
+    let onpageLoad = localStorage.getItem("theme") || "";
+    let element = document.body;
+    element.classList.add(onpageLoad);
+    document.getElementById("theme").textContent =
+      localStorage.getItem("theme") || "light";
+  })();
+  
+  function themeToggle() {
+    let element = document.body;
+    element.classList.toggle("dark");
+  
+    let theme = localStorage.getItem("theme");
+    if (theme && theme === "dark") {
+      localStorage.setItem("theme", "");
+    } else {
+      localStorage.setItem("theme", "dark");
+    }
+  
+    document.getElementById("theme").textContent = localStorage.getItem("theme");
+  }
