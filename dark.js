5337a6f4d58f747d3d71e70c89726c6ad94a0597
Madusanka <42802790+sankalk@users.noreply.github.com>
2022-08-18 13:58:01 +0530

    Update dark.js

--- dark.js
+++ dark.js
@@ -50,5 +50,21 @@ function myFunction() {
   }
 
 // cookies
-
+ var num = 1;
+         function addCookie(){
+            document.cookie = num+" = "+num;
+            num++;
+         }
+         function listCookies(){
+            var result = document.cookie;
+            document.getElementById("list").innerHTML = result;
+         }
+         function removeCookies() {
+            var res = document.cookie;
+            var multiple = res.split(";");
+            for(var i = 0; i < multiple.length; i++) {
+               var key = multiple[i].split("=");
+               document.cookie = key[0]+" =; expires = Thu, 01 Jan 1970 00:00:00 UTC";
+            }
+         }
  
